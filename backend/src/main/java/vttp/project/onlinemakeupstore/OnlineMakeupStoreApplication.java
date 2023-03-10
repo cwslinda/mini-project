53a6fd9afd75b06c865db11ed6b96674584f7d69
cwslinda <lindacheung94@gmail.com>
2023-03-10 14:10:28 +0800

    first-commit

---
+++ backend/src/main/java/vttp/project/onlinemakeupstore/OnlineMakeupStoreApplication.java
@@ -0,0 +1,13 @@
+package vttp.project.onlinemakeupstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMakeupStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMakeupStoreApplication.class, args);
+	}
+
+}
